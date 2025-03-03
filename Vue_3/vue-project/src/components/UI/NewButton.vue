809b090b09e0dc5df7e3c84574a02a17a2fed8da
Barbieri Gianluca (BG5) <gianluca.barbieri@suva.ch>
2025-03-03 13:55:54 +0100

    initialize Vue 3 project structure with TypeScript, Cypress, and Vite configuration

---
+++ Vue_3/vue-project/src/components/UI/NewButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button 
+        :style="{ background: background, color: color }" 
+        :disabled="disable"
+    >
+        Button
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        background: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        disable: {
+            type: Boolean
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue_3/vue-project/src/views/Form.vue
@@ -0,0 +1,20 @@
+<script>
+import NewButton from '../components/UI/NewButton.vue'
+
+export default {
+    components: {
+        NewButton
+    }
+}
+</script>
+
+<template>
+    <new-button
+        background="red" 
+        color="white" 
+        :disable="true"
+    />
+  </template>
+
+<style scoped>
+</style>
