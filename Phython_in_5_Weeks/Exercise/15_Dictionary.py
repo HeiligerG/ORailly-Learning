0b0bc7324f28c1fb8d7397bc675a8d269feb169d
Barbieri Gianluca (BG5) <gianluca.barbieri@suva.ch>
2025-03-11 11:34:48 +0100

    dict

---
+++ Phython_in_5_Weeks/Exercise/15_Dictionary.py
@@ -0,0 +1,19 @@
+total = 0
+
+menu = { 'sandwich':10, 'tea': 8 , 'apple':3, 'cake':10}
+
+while True:
+    order = input('What do you want to order? ').strip() 
+
+    if order == '':
+        break
+
+    if order in menu:
+        price = menu[order]
+        total += price
+        print(f'The {order} is {menu[order]}, your total is now {total}')
+
+    else:
+        print(f'Sorry we are out of {order} today.')
+
+print(f'total is {total}')
